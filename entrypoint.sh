3edd11fe1de8001754ce9991e02acfbcc7d30f3c
GCD+ <127302048+MAJD12358@users.noreply.github.com>
2024-02-20 23:21:29 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Building GX Language Image..."
+# Add any build commands or setup steps you need here
+
+# Run the gx command
+gx
+
+echo "GX Language Image built successfully!"
